863b35b3e83ede492aeb751a8d5856285c643ec6
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-08-17 19:58:06 +0900

    GameOfLifeDivTwo test green

--- Div2/1/GameOfLifeDivTwo/answer.cc
+++ Div2/1/GameOfLifeDivTwo/answer.cc
@@ -7,6 +7,40 @@ using namespace std;
 // GameOfLifeDivTwo
 // http://community.topcoder.com/stat?c=problem_statement&pm=11483
 
+char liveOrDie(string lifes, unsigned int pos) {
+	string threeCells;
+
+	if (pos == 0) {
+		threeCells += lifes[lifes.size() - 1];
+		threeCells += lifes.substr(0, 2);
+	} else if (pos == lifes.size() - 1) {
+		threeCells += lifes.substr(pos - 1, 2);
+		threeCells += lifes[0];
+	} else {
+		threeCells += lifes.substr(pos - 1, 3);
+	}
+
+	int numLives = 0;
+	for (unsigned int i = 0; i < threeCells.size(); i++) {
+		if (threeCells[i] == '1') {
+			numLives++;
+		}
+	}
+
+	return (numLives >= 2) ? '1' : '0';
+}
+
 string GameOfLifeDivTwo::theSimulation(string init, int t) {
-	return init;
+	string now, next;
+
+	now = init;
+	for (int i = 0; i < t; i++) {
+		next = "";
+		for (unsigned int j = 0; j < now.size(); j++) {
+			next += liveOrDie(now, j);
+		}
+		now = next;
+	}
+
+	return now;
 }
