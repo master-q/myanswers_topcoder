a5e203748ceec03e1ecab312fdff3acc8260834a
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-08-17 15:54:08 +0900

    MarbleDecoration test green

--- Div2/1/MarbleDecoration/answer.cc
+++ Div2/1/MarbleDecoration/answer.cc
@@ -1,4 +1,5 @@
 #include "answer.h"
+#include <iostream>
 #include <cmath>
 #include <algorithm>
 using namespace std;
@@ -7,5 +8,19 @@ using namespace std;
 // http://community.topcoder.com/stat?c=problem_statement&pm=11287
 
 int MarbleDecoration::maxLength(int r, int g, int b) {
-	return 0;
+	vector<int> m;
+	m.push_back(r);
+	m.push_back(g);
+	m.push_back(b);
+	sort(m.begin(), m.end());
+
+	if (m[2] == 0) {
+		return 0;
+	} else if (m[1] == 0) {
+		return 1;
+	} else if (m[2] == m[1]) {
+		return m[2] * 2;
+	} else {
+		return m[1] * 2 + 1;
+	}
 }
