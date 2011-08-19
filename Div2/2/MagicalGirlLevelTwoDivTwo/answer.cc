87790b9b4892c9f0e88fcb6d1b29f9e35ff5f69b
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-08-19 17:32:15 +0900

    copy from http://community.topcoder.com/stat?c=problem_solution&cr=22786869&rd=14539&pm=11477

--- Div2/2/MagicalGirlLevelTwoDivTwo/answer.cc
+++ Div2/2/MagicalGirlLevelTwoDivTwo/answer.cc
@@ -7,5 +7,18 @@ using namespace std;
 // http://community.topcoder.com/stat?c=problem_statement&pm=11477
 
 string MagicalGirlLevelTwoDivTwo::isReachable(vector<int> jumpTypes, int x, int y) {
-	return "NO"; // xxx
+	int ou=0;
+	for (vector<int>::iterator j=jumpTypes.begin();j!=jumpTypes.end();j++) {
+		if (*j%2==0) {
+			ou=1;
+		}
+	}
+
+	if (ou==1) {
+		return "YES";
+	} else if ((x+y)%2==0) {
+		return "YES";
+	} else {
+		return "NO";
+	}
 }
